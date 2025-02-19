dcce7abd5c8732c9654cd398e7b8acefe5a22cde
matinsyed <coder.matin@gmail.com>
2025-02-20 00:34:10 +0600

    started

---
+++ Problem-1.js
@@ -0,0 +1,11 @@
+function cashOut( money ) {
+    if(money.NaN){
+        console.log("Not a Number");
+    }
+    else{
+        console.log("Number");
+    }
+    console.log(typeof(money));
+    }
+
+cashOut("asdf");
