8ff27714495c4e8a33b1777b3b609d5e8e35c04a
matinsyed <coder.matin@gmail.com>
2025-02-20 18:56:10 +0600

    problem-2 completed

--- problem-2.js
+++ problem-2.js
@@ -1,8 +1,6 @@
 function validEmail( email ) {
     if(typeof(email) === "string"){ 
-        // console.log(email);
         email = email.trim();
-        // console.log(email);
 
         if((email.startsWith(".") || email.startsWith("-") || email.startsWith("_") || email.startsWith("+") || email.startsWith("@") || email.includes(" "))){
             return false;
@@ -10,22 +8,22 @@ function validEmail( email ) {
         else if(email.endsWith(".com") && email.includes("@")){
             return true;
         }
+        else{
+            return false; 
+        }
     }
     else{
         return "Invalid";
     }
 }
 
-// console.log("JavaScript".includes("Script",4)); // true
 
-// console.log("coder.matin@gmail.com".endsWith(".com")); // true
-
-// // console.log("Hello 123".match(/\d+/)); // ["123"]
-
-// console.log("Hello World".startsWith(/\d+/)); // true
-
-validEmail("      coder.matin@gmail.com");
-validEmail("coder.matingmail.com");
-validEmail(['123']);
-validEmail("coder.matin@gmail.org");
-validEmail("coder.  matin@gmail.com");
+console.log(validEmail("ferdous@gmail.com"));
+console.log(validEmail("1zihad@gmail.com"));
+console.log(validEmail("-king@yahoo.com"));
+console.log(validEmail(["jhankar@hero.com"]));
+console.log(validEmail("Mewo@cat.com"));
+console.log(validEmail("programming-hero.com"));
+console.log(validEmail("chat420@gpt.net"));
+console.log(validEmail(true));
+console.log(validEmail("he ro@alom.com"));
