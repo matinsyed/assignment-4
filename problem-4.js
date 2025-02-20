178876992a40183e1901e454628ccfb74d863885
matinsyed <coder.matin@gmail.com>
2025-02-20 19:24:38 +0600

    proble-4 started

---
+++ problem-4.js
@@ -0,0 +1,20 @@
+function isBestFriend( f1 , f2 ) {
+    
+}
+
+f1 = { 
+    name : " ",
+    roll : 22,
+    bestFriend : 2
+}
+f2 = { 
+    name : " ",
+    roll : 22,
+    bestFriend : 20
+}
+if(f1.bestFriend === f2.roll){
+    console.log("We are best friends");
+}
+else{
+    console.log("We have no friendship");
+}
